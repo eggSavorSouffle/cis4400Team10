a7fcc9c50df351ee5b3b1aba40c663f42b21da45
Xavier Souffront <51895552+eggSavorSouffle@users.noreply.github.com>
2024-04-13 20:38:11 -0400

    sucessfully uploaded fhv july data table

--- scripts/uploading2cloud.py
+++ scripts/uploading2cloud.py
@@ -1,34 +1,32 @@
 from io import StringIO
 from google.cloud import storage
 from google.oauth2 import service_account
+import pandas as pd 
 
+# Credentials for your GCS service account
 credentials = service_account.Credentials.from_service_account_file(r"C:\Users\16466\Desktop\Private Key for team project\deteamproject-40bfde088380.json")
+
+# GCS client with your credentials
 storage_client = storage.Client(credentials=credentials)
 
+# Load the dataset from the provided URL
+df = pd.read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_2023-07.parquet')
 
+# Save the DataFrame to a CSV file
+csv_file_path = 'fhv.csv'
+df.to_csv(csv_file_path)
+
+# Function to upload a file to the GCS bucket
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
-    # The ID of your GCS bucket
-    bucket_name = "de-team-project-bucket"
-    # The path to your file to upload
-    source_file_name = r"C:\Users\16466\Desktop\cis4400Team10\cis4400Team10\data\fhv_tripdata_2023-07.parquet"
-    # The ID of your GCS object
-    destination_blob_name = "destination-blob"
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
     # Optional: set a generation-match precondition to avoid potential race conditions
-    # and data corruptions. The request to upload is aborted if the object's
-    # generation number does not match your precondition. For a destination
-    # object that does not yet exist, set the if_generation_match precondition to 0.
-    # If the destination object already exists in your bucket, set instead a
-    # generation-match precondition using its generation number.
     generation_match_precondition = 0
-
     blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
 
     print(f"File {source_file_name} uploaded to {destination_blob_name}.")
 
-
-upload_blob("de-team-project", r"C:\Users\16466\Desktop\cis4400Team10\cis4400Team10\data\fhv_tripdata_2023-07.parquet", "destination-blob" )
-#insert weather data in this function 
+# Call the upload function with the appropriate arguments
+upload_blob("de-team-project-bucket", csv_file_path, "destination-blob")
