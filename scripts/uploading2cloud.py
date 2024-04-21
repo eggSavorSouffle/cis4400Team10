b241536496310b88e36b63dbeb3208fba662ad7c
Xavier Souffront <51895552+eggSavorSouffle@users.noreply.github.com>
2024-04-20 20:41:59 -0400

    Added connection string, added the remaining tlc files (Did not run it yet), edited data analysis file to see what kind of data I was working with regarding those tlc files, and lastly added the trip record user guide bc I found it to be useful for me in understanding the project better

--- scripts/uploading2cloud.py
+++ scripts/uploading2cloud.py
@@ -6,7 +6,7 @@ import time
 
 # Credentials for GCS service account
 try:
-    credentials = service_account.Credentials.from_service_account_file(r"C:\Users\16466\Desktop\Private Key for team project\deteamproject-40bfde088380.json")
+    credentials = service_account.Credentials.from_service_account_file(r"C:\Users\16466\Desktop\cis4400Team10-1\scripts\Connection String\deteamproject-40bfde088380.json")
     storage_client = storage.Client(credentials=credentials)
     print("GCS client initialized successfully.")
 except Exception as e:
@@ -36,12 +36,35 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name, chunk_size
 
 # List of file URLs and corresponding names for CSV files
 file_urls = [
+    #fhv
     'https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_2023-07.parquet',
     'https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_2023-08.parquet',
     'https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_2023-09.parquet',
     'https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_2023-10.parquet',
     'https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_2023-11.parquet',
-    'https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_2023-12.parquet'
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_2023-12.parquet',
+    #hv
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/fhvhv_tripdata_2023-07.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/fhvhv_tripdata_2023-08.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/fhvhv_tripdata_2023-09.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/fhvhv_tripdata_2023-10.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/fhvhv_tripdata_2023-11.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/fhvhv_tripdata_2023-12.parquet',
+    #yellow
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-07.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-08.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-09.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-10.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-11.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-12.parquet',
+    #green
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-07.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-08.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-09.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-10.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-11.parquet',
+    'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-12.parquet'
+
 
 ]
 
@@ -51,7 +74,29 @@ file_names = [
     'fhv_september.csv',
     'fhv_october.csv',
     'fhv_november.csv',
-    'fhv_december.csv'
+    'fhv_december.csv',
+
+    'hv_july.csv',
+    'hv_august.csv',
+    'hv_september.csv',
+    'hv_october.csv',
+    'hv_november.csv',
+    'hv_december.csv',
+
+    'yellow_july.csv',
+    'yellow_august.csv',
+    'yellow_september.csv',
+    'yellow_october.csv',
+    'yellow_november.csv',
+    'yellow_december.csv',
+
+    'green_july.csv',
+    'green_august.csv',
+    'green_september.csv',
+    'green_october.csv',
+    'green_november.csv',
+    'green_december.csv'
+
 ]
 
 # Process each file URL
@@ -70,5 +115,4 @@ for url, file_name in zip(file_urls, file_names):
         upload_blob("de-team-project-bucket", csv_file_path, file_name)
     except Exception as e:
         print(f"Error with file {file_name}: {e}")
-
 
