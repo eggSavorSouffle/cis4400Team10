efabacbbdb23575fe70667ab91e879e997080de6
Xavier Souffront <xavier.souffront@baruchmail.cuny.edu>
2024-04-26 16:15:30 +0000

    2nd commit getting the hang of dbt more

--- models/dim_base.sql
+++ models/dim_base.sql
@@ -13,3 +13,23 @@ UNION DISTINCT
 SELECT DISTINCT dispatching_base_num, hvfhs_license_num
 FROM deteamproject.TLC_dataset.hv_august
 
+UNION DISTINCT
+
+SELECT DISTINCT dispatching_base_num, hvfhs_license_num
+FROM deteamproject.TLC_dataset.hv_september
+
+UNION DISTINCT
+
+SELECT DISTINCT dispatching_base_num, hvfhs_license_num
+FROM deteamproject.TLC_dataset.hv_october
+
+UNION DISTINCT
+
+SELECT DISTINCT dispatching_base_num, hvfhs_license_num
+FROM deteamproject.TLC_dataset.hv_november
+
+UNION DISTINCT
+
+SELECT DISTINCT dispatching_base_num, hvfhs_license_num
+FROM deteamproject.TLC_dataset.hv_december
+
